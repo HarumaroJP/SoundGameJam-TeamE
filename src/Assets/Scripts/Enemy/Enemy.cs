a875fe55b4650339f0df51c520801c7cdb7efcf0
Takizawa Kenta <kenon919@icloud.com>
2023-09-13 09:32:40 +0900

    スコアカウンターの実装

--- src/Assets/Scripts/Enemy/Enemy.cs
+++ src/Assets/Scripts/Enemy/Enemy.cs
@@ -14,10 +14,31 @@ public class Enemy : MonoBehaviour
 
     public void OnTriggerEnter(Collider other)
     {
+        ScoreCounter counter = GameObject.Find("Score").GetComponent<ScoreCounter>();
+
         if (other.CompareTag("Enemy"))
         {
             //当たったときの処理を書く
             //スコアを加算する
+
+            if (other.gameObject.name == "A")
+            {
+                counter.AddScore(100);
+            }
+
+            if (other.gameObject.name == "B")
+            {
+                counter.AddScore(200);
+            }
+
+            if (other.gameObject.name == "C")
+            {
+                counter.AddScore(300);
+            }
+
         }
+
     }
+
+        
 }
