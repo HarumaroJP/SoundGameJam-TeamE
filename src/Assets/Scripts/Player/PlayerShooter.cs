eee3ca0ab6dab87e5c6e6b9a0333065ab672e258
HarumaroJP <haruto09.game@gmail.com>
2023-09-13 15:34:36 +0900

    プレイヤーのイージングを追加

--- src/Assets/Scripts/Player/PlayerShooter.cs
+++ src/Assets/Scripts/Player/PlayerShooter.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Core.Input;
+using DG.Tweening;
 using UnityEngine;
 
 namespace Player
@@ -13,6 +14,9 @@ namespace Player
         [SerializeField] private AudioSource audioSource;
         [SerializeField] private PlayerMovement playerMovement;
 
+        [SerializeField] private float easeDistance;
+        [SerializeField] private float easeDuration;
+
         private InputEvent shootEvent;
 
         public void Shoot(int index)
@@ -21,6 +25,8 @@ namespace Player
                 return;
 
             audioSource.PlayOneShot(seClips[index]);
+
+            transform.DOLocalMoveX(easeDistance, easeDuration).SetEase(Ease.OutQuad, 2.5f).SetLoops(2, LoopType.Yoyo).Play();
 
             Bullet bullet = Instantiate(bullets[index], transform.position, Quaternion.identity).GetComponent<Bullet>();
             bullet.Shoot();
